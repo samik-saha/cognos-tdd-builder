8ffb1e0af5dc2255b3ba9af6c99122c80d84b0a3
Samik Saha <samiksaha88@gmail.com>
2014-03-30 19:43:44 +0530

    not implemented

---
+++ src/com/cognizant/cognos/tddbuilder/ExcelOutput.java
@@ -0,0 +1,17 @@
+package com.cognizant.cognos.tddbuilder;
+
+public class ExcelOutput implements ExportedOutputInterface {
+
+	@Override
+	public void writePromptDetails(Prompt[] prompt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String writeToFile() {
+		return null;
+		
+	}
+
+}
